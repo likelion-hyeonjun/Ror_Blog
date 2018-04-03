6efdb54567b491c55a85a71b878061ca73272036
LeeHyeonJun <snail1882@naver.com>
2018-04-03 18:39:20 +0900

    모델 관계선언

--- Blog/db/schema.rb
+++ Blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403090144) do
+ActiveRecord::Schema.define(version: 20180403092511) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180403090144) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bulletin_id"
+    t.index ["bulletin_id"], name: "index_posts_on_bulletin_id"
   end
 
 end
