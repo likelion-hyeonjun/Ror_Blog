903de595eaee5f5037372be738e1b18446487c21
LeeHyeonJun <snail1882@naver.com>
2018-04-04 11:01:06 +0900

    Posts 뷰 변경까지 완료

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :bulletins
   resources :posts
+  resources :bulletins do
+    resources :posts
+  end
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
