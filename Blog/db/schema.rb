ab4fa275445c6c25623f52b0b2dec9b970708a29
LeeHyeonJun <snail1882@naver.com>
2018-04-04 11:39:20 +0900

    게시판 레이아웃 작성하기 전 Strong parameter 추가하기까지

--- Blog/db/schema.rb
+++ Blog/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403092511) do
+ActiveRecord::Schema.define(version: 20180404023114) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "post_type"
+    t.integer "post_type_cd", default: 0
   end
 
   create_table "posts", force: :cascade do |t|
